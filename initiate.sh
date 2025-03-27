5bd404c8838da824be2ce0039df6e3a7a04fa18b
Cattette <aramiscohen02@gmail.com>
2025-03-27 13:46:28 +0100

    misc

---
+++ initiate.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+if [ -z $1 ];
+then
+    echo "try 'laptop' or 'pc'"
+else
+    if [ $1 = laptop ]
+    then
+        
+    fi
+
+    if [ $1 = pc ]
+    then
+        echo "ello"
+    fi
+fi
